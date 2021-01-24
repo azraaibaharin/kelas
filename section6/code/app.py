4008fea8df8b285539eae5f9b70f549280ead542
Azraai Baharin <dominoseffect@gmail.com>
2021-01-24 22:51:17 +0800

    Save section 6 tutorials

--- section6/code/app.py
+++ section6/code/app.py
@@ -1,15 +1,23 @@
-from datetime import time, timedelta
+from datetime import timedelta
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from user import UserRegister
-from item import Item, ItemList
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from db import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ayai'
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
@@ -23,11 +31,12 @@ def customized_response_handler(access_token, identity):
         'username': identity.username
     })
 
-items = []
-
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
