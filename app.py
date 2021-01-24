04a003c69b00f130e3075201a339ae61dd42039c
Azraai Baharin <dominoseffect@gmail.com>
2021-01-25 00:19:27 +0800

    Add run py

--- app.py
+++ app.py
@@ -15,10 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ayai'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 jwt = JWT(app, authenticate, identity)
